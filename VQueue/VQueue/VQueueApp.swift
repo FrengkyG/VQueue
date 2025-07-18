c5a5c90ed332efa0ff47e93533611983af92ef88
Frengky Gunawan <frengky.gg@gmail.com>
2025-07-18 10:24:33 +0700

    initial commit

---
+++ VQueue/VQueue/VQueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  VQueueApp.swift
+//  VQueue
+//
+//  Created by Frengky Gunawan on 18/07/25.
+//
+
+import SwiftUI
+
+@main
+struct VQueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
