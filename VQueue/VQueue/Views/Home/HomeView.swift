e9579d1101e8820e38eb75e0887efaf80b923cb4
Frengky Gunawan <frengky.gg@gmail.com>
2025-07-24 10:05:22 +0700

    feat(app): add ui home

--- VQueue/VQueue/Views/Home/HomeView.swift
+++ VQueue/VQueue/Views/Home/HomeView.swift
@@ -16,64 +16,94 @@ struct HomeView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                HStack {
-                    Spacer()
-                    Image("iconBoothLogin")
-                }
+            ZStack {
+                Image("bgHome")
+                    .resizable()
+                    .scaledToFill()
                 
-                Spacer()
-                
-                Button(action: {
-                    isPresentingScanner = true
-                }
-                ){
-                    Text("Start Scan")
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.redColor)
-                        .cornerRadius(12)
-                }
-                .padding(.horizontal, 20)
-                
-                Button(action: {
-                    isNavigatingToQueueList = true
-                }) {
-                    Text("Queue List")
-                        .fontWeight(.semibold)
-                        .foregroundColor(Color.redColor)
-                        .frame(maxWidth: .infinity)
-                        .padding()
-                        .background(Color.white)
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 12)
-                                .stroke(Color.redColor ?? Color.red, lineWidth: 2)
-                        )
-                        .cornerRadius(12)
-                }
-                .padding(.horizontal, 20)
-                .padding(.top, 16)
-            }
-            .padding(.horizontal, 36)
-            .fullScreenCover(isPresented: $isPresentingScanner) {
-                QrScanView { result in
-                    if scannedResult != result {
-                        scannedResult = result
-                        path.append(result)
+                VStack(alignment: .leading) {
+                    HStack {
+                        Spacer()
+                        Image("iconBoothLogin")
                     }
-                    isNavigatingToBooth = true
-                    isPresentingScanner = false
+                    
+                    Group {
+                        HStack {
+                            Text("Welcome to")
+                            Text("GetQ!")
+                                .foregroundColor(.redColor)
+                                .fontWeight(.bold)
+                        }
+                    }
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.black)
+                    .padding(.top, 25)
+                    
+                    Text("Your smart queue companion at JxB. \n")
+                        .padding(.top, 30)
+                    
+                    Text("Queue at up to 3 booths at once, and feel free to shop around while you wait!")
+                    
+                    Image("imagePeopleScan")
+                        .padding(.top, 30)
+
+                    
+                    Button(action: {
+                        isPresentingScanner = true
+                    }
+                    ){
+                        Text("Start Scan")
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.redColor)
+                            .cornerRadius(12)
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    Button(action: {
+                        isNavigatingToQueueList = true
+                    }) {
+                        Text("Queue List")
+                            .fontWeight(.semibold)
+                            .foregroundColor(Color.redColor)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.white)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(Color.redColor ?? Color.red, lineWidth: 2)
+                            )
+                            .cornerRadius(12)
+                    }
+                    .padding(.horizontal, 20)
+                    .padding(.top, 16)
                 }
+                .fullScreenCover(isPresented: $isPresentingScanner) {
+                    QrScanView { result in
+                        if scannedResult != result {
+                            scannedResult = result
+                            path.append(result)
+                        }
+                        isNavigatingToBooth = true
+                        isPresentingScanner = false
+                    }
+                }
+                .navigationDestination(isPresented: $isNavigatingToBooth) {
+                    BoothView()
+                }
+                .navigationDestination(isPresented: $isNavigatingToQueueList) {
+                    QueueListView()
+                }
+                .padding(.horizontal, 36)
+                .padding(.vertical, 37)
+
             }
-            .navigationDestination(isPresented: $isNavigatingToBooth) {
-                BoothView()
-            }
-            .navigationDestination(isPresented: $isNavigatingToQueueList) {
-                QueueListView()
-            }
+            
         }
+        
     }
 }
 
